ee817a56519f72e89a32801d5f8ebc92509dc40e
Santos <sjimenez77@gmail.com>
2014-01-02 19:11:53 +0100

    Comienzo de plantilla de portada
    - Corregida la acción del controlador

--- src/ROV/StartBundle/Controller/DefaultController.php
+++ src/ROV/StartBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function homeAction()
     {
-        return $this->render('ROVStartBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ROVStartBundle:Default:index.html.twig');
     }
 }
