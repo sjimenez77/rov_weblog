9580c423f5b1554ac474f8429f1096320f91d7db
Santos <sjimenez77@gmail.com>
2014-03-06 00:27:58 +0100

    Problemas con intl

--- web/js/compiled/main.js
+++ web/js/compiled/main.js
@@ -15914,3 +15914,17 @@ the specific language governing permissions and limitations under the Apache Lic
     };
 
 }(jQuery));
+
+var amplia_footer = function() {
+    // Ampliamos el alto del footer hasta el fondo de la página en el caso
+    // de que el contenido sea demasiado pequeño y el footer se quede por 
+    // encima del mismo
+    var diferencia = $(window).height() - ($('footer').height() + $('footer').offset().top);
+    if (diferencia > 0) {
+        var altura_footer = $('footer').height();
+        $('footer').height(altura_footer + diferencia - 50);
+    } else {
+        $('footer').height('auto');
+    }
+    return true;
+};
