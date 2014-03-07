c337526c8ecc95273a7d2b797031f17db55eecc7
Santos Jiménez Linares <sjimenez77@gmail.com>
2014-03-07 10:39:02 +0100

    Clase Util para funciones comunes como getSlug

---
+++ src/ROV/BlogBundle/Util/Util.php
@@ -0,0 +1,17 @@
+<?php
+// src/ROV/BlogBundle/Util/Util.php
+namespace ROV\BlogBundle\Util;
+ 
+class Util
+{
+    static public function getSlug($cadena, $separador = '-')
+    {
+        // Código copiado de http://cubiq.org/the-perfect-php-clean-url-generator
+        $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $cadena);
+        $slug = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $slug);
+        $slug = strtolower(trim($slug, $separador));
+        $slug = preg_replace("/[\/_|+ -]+/", $separador, $slug);
+        
+        return $slug;
+    }
+}
