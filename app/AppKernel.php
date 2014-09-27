84b3de9974ed3587fae36f4671e6720ec282044b
Santos <sjimenez77@gmail.com>
2014-09-27 20:34:42 +0200

    Vich\UploadBundle incorporado y configurado
    - Permite uploads sencillos adjuntos a entidades.
    - Mas info en
    https://github.com/dustin10/VichUploaderBundle/blob/master/Resources/doc
    /index.md

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
             new ROV\BlogBundle\ROVBlogBundle(),
             new ROV\UsersBundle\ROVUsersBundle(),
             new ROV\StartBundle\ROVStartBundle(),
